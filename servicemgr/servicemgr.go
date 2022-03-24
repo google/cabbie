2911c70b36d6deb78a870f7a1b3f9b37b6e0c00f
Matt LaPlante <mattl@google.com>
2022-03-24 13:18:43 -0700

    Add non-Windows stubs for the servicemgr, updatecollection, & updates packages.
    PiperOrigin-RevId: 437070658

--- servicemgr/servicemgr.go
+++ servicemgr/servicemgr.go
@@ -17,9 +17,7 @@
 package servicemgr
 
 import (
-	"github.com/google/cabbie/cablib"
 	"github.com/go-ole/go-ole"
-	"github.com/go-ole/go-ole/oleutil"
 )
 
 // ServiceID indicates which update source is being scanned. More info and common
@@ -50,57 +48,3 @@ const (
 	// WSUS ServiceID.
 	WSUS ServiceID = "3DA21691-E39D-4da6-8A4B-B43877BCB1B7"
 )
-
-// InitMgrService creates an update service manager object.
-func InitMgrService() (*ServiceManager, error) {
-	mgr, err := cablib.NewCOMObject("Microsoft.Update.ServiceManager")
-	if err != nil {
-		return nil, err
-	}
-	return &ServiceManager{ServiceManager: mgr}, nil
-}
-
-// AddService registers a service with Windows Update Agent (WUA) without requiring an authorization
-// cabinet file (.cab).
-// More info can be found at https://docs.microsoft.com/en-us/windows/win32/api/wuapi/nf-wuapi-iupdateservicemanager2-addservice2
-func (m *ServiceManager) AddService(s ServiceID) error {
-	_, err := oleutil.CallMethod(m.ServiceManager, "AddService2", string(s), 7, "")
-	return err
-}
-
-// QueryServiceRegistration verifies if a serviceID has been registered with Windows Update Agent.
-func (m *ServiceManager) QueryServiceRegistration(s ServiceID) (bool, error) {
-	sr, err := oleutil.CallMethod(m.ServiceManager, "QueryServiceRegistration", string(s))
-	if err != nil {
-		return false, err
-	}
-	srd := sr.ToIDispatch()
-	defer srd.Release()
-
-	state, err := oleutil.GetProperty(srd, "RegistrationState")
-	if err != nil {
-		return false, err
-	}
-	defer state.Clear()
-
-	// Possible state values:
-	// 1 = The service is not registered.
-	// 2 = The service is pending registration.
-	// 3 = The service is registered.
-	if state.Val == registered {
-		return true, nil
-	}
-
-	return false, nil
-}
-
-// RemoveService removes a service registration from Windows Update Agent (WUA).
-func (m *ServiceManager) RemoveService(s ServiceID) error {
-	_, err := oleutil.CallMethod(m.ServiceManager, "RemoveService", string(s))
-	return err
-}
-
-// Close turns down any open service manager sessions.
-func (m *ServiceManager) Close() {
-	m.ServiceManager.Release()
-}
