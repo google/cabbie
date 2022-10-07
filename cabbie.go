2bf3816e4ab205ac5779d2c5b292948be7694b6f
Brian Garber <bagarber@google.com>
2022-10-07 10:11:15 -0700

    Remove obsolete bug.
    PiperOrigin-RevId: 479610853

--- cabbie.go
+++ cabbie.go
@@ -581,7 +581,6 @@ func main() {
 	defer comshim.Done()
 
 	// Running as Service.
-	// TODO(b/147692789): move service logic into its own subcommand.
 	if isSvc && len(os.Args) == 1 {
 		if err := startService(*runInDebug); err != nil {
 			elog.Error(cablib.EvtErrService, fmt.Sprintf("Failed to run service: %v", err))
