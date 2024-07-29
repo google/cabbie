3de01532697909fe2e83635306be82a78071f3f6
Cabbie Team <noreply@google.com>
2024-07-29 09:50:46 -0700

    Modify cabbie's update search behavior to not return when it has an error parsing an update's metadata, instead log and continue. This helps for cases where MS fails to set update metadata on certain KBs.
    PiperOrigin-RevId: 657233213

--- search/search_windows.go
+++ search/search_windows.go
@@ -28,7 +28,6 @@ import (
 	"github.com/google/cabbie/updatecollection"
 	"github.com/google/cabbie/updates"
 	"github.com/google/cabbie/wsus"
-	"github.com/google/deck"
 	"golang.org/x/sys/windows/registry"
 	"github.com/go-ole/go-ole"
 	"github.com/go-ole/go-ole/oleutil"
@@ -130,7 +129,7 @@ func (s *Searcher) QueryUpdates() (*updatecollection.Collection, error) {
 
 		up, errors := updates.New(itemd)
 		if errors != nil {
-			deck.Errorf("errors in update enumeration: %v", errors)
+			return nil, fmt.Errorf("errors in update enumeration: %v", errors)
 		}
 		updd.Updates[i] = up
 
